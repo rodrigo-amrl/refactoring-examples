39e0129a99e9d683ed4e059d53adc674a7dab7df
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-01-06 16:04:52 -0300

    ajust move statemants

--- tests/unit/CatalogRefactoring/MoveStatementsToCallersTest.php
+++ tests/unit/CatalogRefactoring/MoveStatementsToCallersTest.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\CatalogRefactoring\MoveStatementsToCallers\PersonRefactored;
 use PHPUnit\Framework\TestCase;
 
 final class MoveStatementsToCallersTest extends TestCase
 {
 
-    public function test()
+    public function testRenderPerson()
     {
+        $photo = ['title' => "Vacation", "location" => "Las Vegas", "2025-05-10"];
+        $person_data = ['name' => "Rodrigo", 'photo' => $photo];
+        $person = new PersonRefactored();
+
+        $this->expectOutputRegex("/{$photo['title']}/");
+        $person->renderPerson($person_data);
+
     }
 }
